1a7475114a7ce4b053489b4460cd03f805d19acb
hariprashantb <harry.prashantb@gmail.com>
2021-01-27 22:10:17 +0530

    images: models: Add Category and Image models.

--- images/models.py
+++ images/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+import uuid
 
 # Create your models here.
+
+def user_directory_path(instance, filename):
+    return 'images/{0}/'.format(filename)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+OPTIONS = (
+        ('active', _('Active')),
+        ('deactivated', _('Deactivated'))
+    )
+
+class Image(models.Model):
+    _id = models.UUIDField(default=uuid.uuid4, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=250)
+    alt = models.TextField(null=True) #Alternate text
+    image = models.ImageField(
+        upload_to=user_directory_path, default='posts/default.jpg'
+    )
+    slug = models.SlugField(max_length=250, unique_for_date='created')
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, 
+                               related_name='author')
+    status = models.CharField(max_length=11, choices=OPTIONS, default='active')
