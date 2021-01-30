1d8266a6dad561192ba4daf4c216037861e2ffa6
Hari <harry.prashantb@gmail.com>
2021-01-30 19:22:23 +0530

    api/urls: Route URLs for ImageAPIView.

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import ImageAPIView
 
 urlpatterns = [
-    path('id/<id>/', ImageAPIView.as_view())
+    path('id/<id>/', ImageAPIView.as_view()),
+    path('upload/', ImageAPIView.as_view()),
 ]
